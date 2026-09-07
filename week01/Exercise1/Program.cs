9126e8b0a14ae7a51318eba00d329edb24254336
Sherelton B. Dennis <shereltondennis@gmail.com>
2026-09-07 23:55:36 +0000

    week01 exercise is done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine(firstName);
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($" Your full name is: {lastName}, {firstName} {lastName}");
+
+
     }
 }
