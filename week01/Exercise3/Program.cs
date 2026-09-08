18717aa78c9783982846a5446d9fd4c8f8ab89bb
Sherelton B. Dennis <shereltondennis@gmail.com>
2026-09-08 00:22:01 +0000

    exercise 03 is done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = Random.Shared.Next(1, 101);
+        int guess;
+
+        Console.WriteLine("Guess the magic number between 1 and 100!");
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed it!");
+            }
+        }
+        while (guess != magicNumber);
     }
 }
